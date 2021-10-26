19906b055866931bf9bda42fd620b131eef9cf3e
Ilya <iliya.merkulov@mail.ru>
2021-10-26 19:31:26 +0500

    В файл записываются русские буквы

--- LanguageDictionary/FileManager.cs
+++ LanguageDictionary/FileManager.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace LanguageDictionary
@@ -26,9 +28,14 @@ namespace LanguageDictionary
             {
                 { word,translations}
             };
-            string jsonString = JsonSerializer.Serialize(languageDictionary);
+            var options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(languageDictionary, options);
             Console.WriteLine(jsonString);
-            File.AppendAllText(@$"{path}\{file}", jsonString, Encoding.Unicode);
+            File.AppendAllText(@$"{path}\{file}", jsonString);
         }
 
 
