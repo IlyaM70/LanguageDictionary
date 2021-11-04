a729f88a56955a03ec47cdc17e6c437587a7c843
Ilya <iliya.merkulov@mail.ru>
2021-11-04 16:19:33 +0500

    Добавление класса ConsoleApp

---
+++ LanguageDictionary/ConsoleApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageDictionary
+{
+    public static class ConsoleApp
+    {
+        public static string MainMenu()
+        {
+            Console.WriteLine("Словарь разных языков");
+            Console.WriteLine("Выберите действие");
+            Console.WriteLine("1.Создать словарь");
+            Console.WriteLine("2.Выполнить действие со словарем");
+            Console.WriteLine("0.Выйти");
+            var choise = Console.ReadLine();
+            return choise;
+        }
+    }
+}
