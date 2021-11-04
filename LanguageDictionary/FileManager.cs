be584c70e61d9a8908e762fd176af34b9a353b01
Ilya <iliya.merkulov@mail.ru>
2021-11-04 16:19:17 +0500

    Сокращение кода

--- LanguageDictionary/FileManager.cs
+++ LanguageDictionary/FileManager.cs
@@ -1,19 +1,21 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Text.Unicode;
-using System.Threading.Tasks;
+
 
 
 namespace LanguageDictionary
 {
     public static class FileManager
     {
+        public static void Create(string languageFrom, string languageTo, string path)
+        {
+            var languageDictionary = new Dictionary<string, List<string>>();
+            string jsonString = JsonSerializer.Serialize(languageDictionary);
+            File.WriteAllText(@$"{path}\{languageFrom}-{languageTo}.json", jsonString);
+        }
         public static void GetFileList(string path)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
@@ -23,32 +25,12 @@ namespace LanguageDictionary
                 Console.WriteLine(file.Name);
             }
         }
-
-        //public static void Write(string file, string path, string word, List<string> translations)
-        //{
-        //    var languageDictionary = new Dictionary<string, List<string>>()
-        //    {
-        //        { word,translations}
-        //    };
-        //    var options = new JsonSerializerOptions
-        //    {
-        //        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-        //        WriteIndented = true,
-        //        AllowTrailingCommas = true
-        //    };
-        //    string jsonString = System.Text.Json.JsonSerializer.Serialize(languageDictionary, options);
-        //    //string jsonString = JsonConvert.SerializeObject(languageDictionary, Formatting.Indented);
-
-        //    //Console.WriteLine(jsonString);
-        //    File.AppendAllText(@$"{path}\{file}", jsonString);
-        //}
-
+        
         public static Dictionary<string, List<string>> Read (string filePath)
         {
             var jsonString = File.ReadAllText(filePath);
-            var dictionary = new Dictionary<string, List<string>>();
-            return dictionary = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, List<string>>>(jsonString);
-            //return dictionary = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(jsonString);
+            return JsonSerializer.Deserialize<Dictionary<string, List<string>>>(jsonString);
+            
         }
 
         public static void Replace(string file, string path, Dictionary<string, List<string>> dictionary )
@@ -60,9 +42,7 @@ namespace LanguageDictionary
                 WriteIndented = true,
                 AllowTrailingCommas = true
             };
-            string jsonString = System.Text.Json.JsonSerializer.Serialize(dictionary, options);
-            //string jsonString = JsonConvert.SerializeObject(languageDictionary, Formatting.Indented);
-            //Console.WriteLine(jsonString);
+            string jsonString = JsonSerializer.Serialize(dictionary, options);
             File.WriteAllText(@$"{path}\{file}", jsonString);
         }
 
--- LanguageDictionary/Program.cs
+++ LanguageDictionary/Program.cs
@@ -11,19 +11,7 @@ namespace LanguageDictionary
         static void Main(string[] args)
         {
             var path = @$"{Environment.CurrentDirectory}\dictionaries";
-            Console.WriteLine("Словарь разных языков");
-            Console.WriteLine("Выберите действие");
-            Console.WriteLine("1.Создать словарь");
-            Console.WriteLine("2.Добавить слово и перевод в существующий словарь");
-            Console.WriteLine("3.Заменить слово или его перевод в словаре");
-            Console.WriteLine("4.Удалить слово или его перевод в словаре");
-            Console.WriteLine("5.Искать перевод слова");
-            Console.WriteLine("6.Записать слово и его перевод в файл");
-            Console.WriteLine("0.Выйти");
-            var choise = Console.ReadLine();
-
-            
-
+            var choise = ConsoleApp.MainMenu();
             switch (choise)
             {
                 case "1":
@@ -31,7 +19,7 @@ namespace LanguageDictionary
                     var languageFrom = Console.ReadLine();
                     Console.WriteLine("Напишите на какой язык будет осуществляться перевод");
                     var languageTo = Console.ReadLine();
-                    var languageDictionary = new LanguageDictionary(languageFrom,languageTo,path);
+                    FileManager.Create(languageFrom, languageTo, path);
                     break;
                 case "2":
 
@@ -40,74 +28,30 @@ namespace LanguageDictionary
                     Console.WriteLine("Введите имя файла");
                     var file = Console.ReadLine();
                     var filePath = @$"{path}\{file}";
-                    //Console.WriteLine(filePath);
+                    
 
                     //// get dictionary
                     var dictionary = FileManager.Read(filePath);
 
-                    /// get word and tranlations
-                    Console.WriteLine("Напишите новое слово");
-                    var word = Console.ReadLine();
-                    Console.WriteLine("Напишите перевод");
-                    var tranlate = Console.ReadLine();
-                    var translations = new List<string>();
-                    translations.Add(tranlate);
-                    var flag = true;
-                    while(flag)
-                    {
-                        Console.WriteLine("Напишите дополнительный перевод или 0 для выхода");
-                        tranlate = Console.ReadLine();
-                        if (tranlate == "0")
-                        {
-                            flag = false;
-                            dictionary.Add(word, translations);
-                        }
-                        else
-                        {
-                            translations.Add(tranlate);
-                            dictionary.Add(word, translations);
-                        }
-                    }
-                    //Console.WriteLine(dictionary);
-                    FileManager.Replace(file, path, dictionary);
-                    break;
 
-                    
-                case "3":
-                    FileManager.GetFileList(path);
-                    Console.WriteLine("Введите имя файла");
-                    file = Console.ReadLine();
-                    filePath = @$"{path}\{file}";
-
-                    dictionary = FileManager.Read(filePath);
-
-
-                    Console.WriteLine("Введите слово");
-                    word = Console.ReadLine();
-
-                    Console.WriteLine("Выберите вариант");
-                    Console.WriteLine("1.Заменить слово");
-                    Console.WriteLine("2.Заменить перевод слова");
+                    Console.WriteLine("Выберите действие");
+                    Console.WriteLine("1.Добавить слово и перевод в существующий словарь");
+                    Console.WriteLine("2.Заменить слово или его перевод в словаре");
+                    Console.WriteLine("3.Удалить слово или его перевод в словаре");
+                    Console.WriteLine("4.Искать перевод слова");
+                    Console.WriteLine("5.Записать слово и его перевод в файл");
                     choise = Console.ReadLine();
-                    
-
                     switch (choise)
                     {
                         case "1":
-                            translations = dictionary[word];
-                            dictionary.Remove(word);
-                            Console.WriteLine("Введите новое слово");
-                            word = Console.ReadLine();
-                            dictionary.Add(word, translations);
-                            break;
-
-                        case "2":
-                            dictionary.Remove(word);
-                            Console.WriteLine("Введите новый перевод");
-                            tranlate = Console.ReadLine();
-                            translations = new List<string>();
+                            /// get word and tranlations
+                            Console.WriteLine("Напишите новое слово");
+                            var word = Console.ReadLine();
+                            Console.WriteLine("Напишите перевод");
+                            var tranlate = Console.ReadLine();
+                            var translations = new List<string>();
                             translations.Add(tranlate);
-                            flag = true;
+                            var flag = true;
                             while (flag)
                             {
                                 Console.WriteLine("Напишите дополнительный перевод или 0 для выхода");
@@ -123,103 +67,132 @@ namespace LanguageDictionary
                                     dictionary.Add(word, translations);
                                 }
                             }
+                            //Console.WriteLine(dictionary);
+                            FileManager.Replace(file, path, dictionary);
                             break;
-                    }
 
-                    FileManager.Replace(file, path, dictionary);
-                    break;
-
-                case "4":
-                    ///// get file
-                    FileManager.GetFileList(path);
-                    Console.WriteLine("Введите имя файла");
-                    file = Console.ReadLine();
-                    filePath = @$"{path}\{file}";
-                    
-
-                    //// get dictionary
-                    dictionary = FileManager.Read(filePath);
-
-                    Console.WriteLine("Введите слово");
-                    word = Console.ReadLine();
-
-                    Console.WriteLine("Выберите вариант");
-                    Console.WriteLine("1.Удалить слово");
-                    Console.WriteLine("2.Удалить перевод слова");
-                    choise = Console.ReadLine();
-
-
-                    switch (choise)
-                    {
-                        case "1":
-                            dictionary.Remove(word);
-                            break;
 
                         case "2":
-                            dictionary.Remove(word);
-                            translations = new List<string>();
-                            dictionary.Add(word, translations);
+                            Console.WriteLine("Введите слово");
+                            word = Console.ReadLine();
+
+                            Console.WriteLine("Выберите вариант");
+                            Console.WriteLine("1.Заменить слово");
+                            Console.WriteLine("2.Заменить перевод слова");
+                            choise = Console.ReadLine();
+
+
+                            switch (choise)
+                            {
+                                case "1":
+                                    translations = dictionary[word];
+                                    dictionary.Remove(word);
+                                    Console.WriteLine("Введите новое слово");
+                                    word = Console.ReadLine();
+                                    dictionary.Add(word, translations);
+                                    break;
+
+                                case "2":
+                                    dictionary.Remove(word);
+                                    Console.WriteLine("Введите новый перевод");
+                                    tranlate = Console.ReadLine();
+                                    translations = new List<string>();
+                                    translations.Add(tranlate);
+                                    flag = true;
+                                    while (flag)
+                                    {
+                                        Console.WriteLine("Напишите дополнительный перевод или 0 для выхода");
+                                        tranlate = Console.ReadLine();
+                                        if (tranlate == "0")
+                                        {
+                                            flag = false;
+                                            dictionary.Add(word, translations);
+                                        }
+                                        else
+                                        {
+                                            translations.Add(tranlate);
+                                            dictionary.Add(word, translations);
+                                        }
+                                    }
+                                    break;
+                            }
+
+                            FileManager.Replace(file, path, dictionary);
                             break;
+
+                        case "3":
+                            Console.WriteLine("Введите слово");
+                            word = Console.ReadLine();
+
+                            Console.WriteLine("Выберите вариант");
+                            Console.WriteLine("1.Удалить слово");
+                            Console.WriteLine("2.Удалить перевод слова");
+                            choise = Console.ReadLine();
+
+
+                            switch (choise)
+                            {
+                                case "1":
+                                    dictionary.Remove(word);
+                                    break;
+
+                                case "2":
+                                    dictionary.Remove(word);
+                                    translations = new List<string>();
+                                    dictionary.Add(word, translations);
+                                    break;
+                            }
+
+                            FileManager.Replace(file, path, dictionary);
+                            break;
+
+                        case "4":
+
+                            Console.WriteLine("Введите слово");
+                            word = Console.ReadLine();
+                            translations = new List<string>();
+                            translations = dictionary.GetValueOrDefault(word);
+
+                            Console.WriteLine("Перевод:");
+                            foreach (var translate in translations)
+                            {
+                                Console.WriteLine(translate);
+                            }
+
+                            break;
+                        case "5":
+                                               
+                            Console.WriteLine("Введите слово");
+                            word = Console.ReadLine();
+                            translations = new List<string>();
+                            translations = dictionary.GetValueOrDefault(word);
+
+                            Console.WriteLine("Перевод:");
+                            foreach (var translate in translations)
+                            {
+                                Console.WriteLine(translate);
+                            }
+
+                            File.WriteAllText($@"{Environment.CurrentDirectory}/перевод.txt", $"{word}:");
+                            foreach (var translate in translations)
+                            {
+                                File.AppendAllText($@"{Environment.CurrentDirectory}/перевод.txt", $"{translate}, ");
+                            }
+                            Console.WriteLine("Перевод записан в перевод.txt");
+
+                            break;
+
+
                     }
 
-                    FileManager.Replace(file, path, dictionary);
                     break;
 
-                case "5":
-
-                    FileManager.GetFileList(path);
-                    Console.WriteLine("Введите имя файла");
-                    file = Console.ReadLine();
-                    filePath = @$"{path}\{file}";
-
-                    dictionary = FileManager.Read(filePath);
-
-
-                    Console.WriteLine("Введите слово");
-                    word = Console.ReadLine();
-                    translations = new List<string>();
-                    translations = dictionary.GetValueOrDefault(word);
-
-                    Console.WriteLine("Перевод:");
-                    foreach (var translate in translations)
-                    {
-                        Console.WriteLine(translate);
-                    }
-
-                    break;
-                case "6":
-
-                    FileManager.GetFileList(path);
-                    Console.WriteLine("Введите имя файла");
-                    file = Console.ReadLine();
-                    filePath = @$"{path}\{file}";
-
-                    dictionary = FileManager.Read(filePath);
-
-
-                    Console.WriteLine("Введите слово");
-                    word = Console.ReadLine();
-                    translations = new List<string>();
-                    translations = dictionary.GetValueOrDefault(word);
-
-                    Console.WriteLine("Перевод:");
-                    foreach (var translate in translations)
-                    {
-                        Console.WriteLine(translate);
-                    }
-                   
-                    File.WriteAllText($@"{Environment.CurrentDirectory}/перевод.txt", $"{word}:");
-                    foreach (var translate in translations)
-                    {
-                        File.AppendAllText($@"{Environment.CurrentDirectory}/перевод.txt", $"{translate}, ");
-                    }
-                    Console.WriteLine("Перевод записан в перевод.txt");
-
-                    break;
                 case "0":
                     break;
-                
             }
+           
+
+            
 
         }
     }
